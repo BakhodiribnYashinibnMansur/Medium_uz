5a01b52954f6374178ae31f545be7da59d784bc7
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-06-08 21:39:15 +0500

    set-up heroku

--- util/cors/cors.go
+++ util/cors/cors.go
@@ -6,7 +6,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control,   application/json,X-Requested-With, multipart/form-data")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control,   X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT , DELETE ,PATCH, HEAD")
 
 		if ctx.Request.Method == "OPTIONS" {
