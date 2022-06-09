23cf83b6929d80b032831fedbccf039dd54e00eb
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-06-09 05:41:55 +0500

    set-up heroku

--- util/cors/cors.go
+++ util/cors/cors.go
@@ -9,12 +9,10 @@ func CORSMiddleware() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT , DELETE ,PATCH, HEAD")
-
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(204)
 			return
 		}
-
 		ctx.Next()
 	}
 }
